e3e8a61f90f364f2163bc26459c812567780252e
LeoValik <troval12989@gmail.com>
2019-06-11 15:33:59 +0300

    Almost done

--- js/index.js
+++ js/index.js
@@ -1,45 +1,65 @@
 const result = document.getElementById('calc');
 
 function getResult() {
-    const rate = document.getElementById('rate').value;
+    let rate = document.getElementById('rate').value;
     const sold = document.getElementById('sold').value;
     const profit = document.getElementById('profit').value;
     const change = document.getElementById('change').value;
 
-    // console.log(rate);
-    // console.log(sold);
-    // console.log(profit);
-    // console.log(change);
+    rate = Number(rate);
     let i = 0;
-    while (i <= 1000) {
+    let sum = 0;
 
-        // 2 столбец
-        let sum = 0;
-        sum += Number(rate);
+    document.write(`
+    <style>
+        #result, #resultjs {
+            width: 100%;
+            text-align: center;
+        }
 
-        // 3 столбец
-        let umnog = rate * sold;
-
-        // 4 столбец
-        let ourProfit = umnog - sum;
-
-
-
-        
-
-
-    document.getElementById('resultjs').innerHTML = `
+        th {
+            font-size: 24px;
+        }
+    </style>
+    `);
+    document.write("<table id='result' border='1'>");
+    document.write(`
+    <thead>
     <tr>
-        <td>${rate}</td>
-        <td>${sum.toFixed(8)}</td>
-        <td>${umnog.toFixed(8)}</td>
-        <td>${ourProfit.toFixed(8)}</td>
+        <th>Ставка</th>
+        <th>Потрачено</th>
+        <th>Умножение</th>
+        <th>Профит</th>
     </tr>
-    `;
-    i++;
+    </thead>
+    `);
+    document.write('<tbody id="resultjs">');
+    while (i <= 20) {
+        if(i) {
+            rate += Number(rate);
+        }
+            for(let j = 0; j < change; j++) {
+            // 2 столбец
+            sum += Number(rate);
+        // 3 столбец
+            let umnog = rate * sold;
+            // 4 столбец
+            let ourProfit = umnog - sum;
+
+            document.write(`
+            <tr>
+                <td>${rate.toFixed(8)}</td>
+                <td>${sum.toFixed(8)}</td>
+                <td>${umnog.toFixed(8)}</td>
+                <td>${ourProfit.toFixed(8)}</td>
+            </tr>               
+            `);
+
+        }
+
+        i++;
     }
-
-
+    document.write("</table>")
 }
 
 result.onclick = getResult;
