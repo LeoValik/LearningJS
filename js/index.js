ca38f486edc14bb286725aaf51d4329bd604288c
LeoValik <troval12989@gmail.com>
2019-06-11 17:38:26 +0300

    Done

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 const result = document.getElementById('calc');
 
 function getResult() {
+    // Вводимые переменные
     let rate = document.getElementById('rate').value;
     const sold = document.getElementById('sold').value;
     const profit = document.getElementById('profit').value;
     const change = document.getElementById('change').value;
 
+    // Начальные значения
     rate = Number(rate);
     let i = 0;
     let sum = 0;
 
+    // Переменные файла
+    let file;
+    let data = [];
+
+    // Вставка данных в файл 
+    data.push(" Ставка | Потрачено | Умножение | Профит\r\n");
+
+    // Стили для таблицы
     document.write(`
     <style>
         #result, #resultjs {
@@ -20,8 +30,19 @@ function getResult() {
         th {
             font-size: 24px;
         }
+
+        #link {
+            font-size: 24px;
+            text-align: center;
+            display: block;
+            text-decoration: none;
+        }
     </style>
     `);
+
+    document.write(`<a id="link" target="_blank" download="result.txt">Скачать файл</a>`);
+    document.write('<br>');
+    //Таблица
     document.write("<table id='result' border='1'>");
     document.write(`
     <thead>
@@ -34,6 +55,7 @@ function getResult() {
     </thead>
     `);
     document.write('<tbody id="resultjs">');
+    // Логика всего проекта
     while (i <= 20) {
         if(i) {
             rate += Number(rate);
@@ -41,11 +63,12 @@ function getResult() {
             for(let j = 0; j < change; j++) {
             // 2 столбец
             sum += Number(rate);
-        // 3 столбец
+            // 3 столбец
             let umnog = rate * sold;
             // 4 столбец
             let ourProfit = umnog - sum;
 
+            // Вывод таблицы
             document.write(`
             <tr>
                 <td>${rate.toFixed(8)}</td>
@@ -54,12 +77,26 @@ function getResult() {
                 <td>${ourProfit.toFixed(8)}</td>
             </tr>               
             `);
-
+            // Вывод в файл
+            data.push(`${rate.toFixed(8)} | ${sum.toFixed(8)} | ${umnog.toFixed(8)} | ${ourProfit.toFixed(8)}\r\n`);
         }
 
         i++;
     }
     document.write("</table>")
+
+    // Работа с файлом
+    
+    let properties = {type: 'text/plain'}; // Specify the file mime-type.
+    try {
+    // Specify the filename using the File constructor, but ...
+    file = new File(data, "result.txt", properties);
+    } catch (e) {
+    // ... fall back to the Blob constructor if that isn't supported.
+    file = new Blob(data, properties);
+    }
+    let url = URL.createObjectURL(file);
+    document.getElementById('link').href = url;
 }
 
 result.onclick = getResult;
